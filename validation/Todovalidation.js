625161164de2cd01b1c2b0a12bffb6dc8cabd57d
shiva kumar <115865333+SHIVAKUMAR-KS@users.noreply.github.com>
2024-04-07 13:12:07 +0530

    Delete validation directory

--- validation/Todovalidation.js
+++
@@ -1,9 +0,0 @@
-import zod from 'zod'
-
-const todovalidationSchema =zod.object({
-    title: zod.string(),
-    description: zod.string(),
-    completed: zod.boolean()
-})
-
-export default todovalidationSchema;
