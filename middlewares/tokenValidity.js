41f79f71b03c51327bfcfcde55a48aa95ad7fdf3
SHIVAKUMAR-KS <kshiva09283@gmail.com>
2024-04-07 20:40:35 +0530

    Updated middleware

--- middlewares/tokenValidity.js
+++ middlewares/tokenValidity.js
@@ -2,25 +2,26 @@ import jwt from 'jsonwebtoken';
 import dotenv from 'dotenv';
 dotenv.config();
 
-const tokenValidity = (req , res , next) => {
+const tokenValidity = (req, res, next) => {
     const token = req.headers.authorization;
 
-    if(!token){
-        return res.json({status: 400 , msg: "Access denied. No Tokens provided."});
+    if (!token) {
+        return res.json({ status: 400, msg: "Access denied. No Tokens provided." });
     }
-    
+
     try {
-        const tokenVerify = jwt.verify(token , process.env.PASSWORD);
-        if(tokenVerify){
-            const tokenValidity = tokenVerify.exp <= Date.now() / 1000;
-            if(!tokenValidity){
-                return res.json({status: 200, msg:"Page request granted."});
-            }
-            req.user = tokenVerify; 
+        const tokenVerify = jwt.verify(token, process.env.PASSWORD);
+        if (!tokenVerify) {
+            return res.json({ status: 411, msg: 'Invalid token.' });
         }
+        
+        // Set user information in request object
+        req.user = tokenVerify;
+
+        // Call next middleware
         next();
-    } catch(error){
-        return res.json({status:411, msg: 'Invalid token.' });
+    } catch (error) {
+        return res.json({ status: 411, msg: 'Invalid token.' });
     }
 }
 
