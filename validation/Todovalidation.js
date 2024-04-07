358f2ffae802639f149590407bc99b9913f8d5ce
SHIVAKUMAR-KS <kshiva09283@gmail.com>
2024-04-07 21:11:00 +0530

    Chechked all routes

--- validation/Todovalidation.js
+++ validation/Todovalidation.js
@@ -1,9 +1,9 @@
 import zod from 'zod';
 
-const todoValidationSchema = zod.object({
+export const todoValidationSchema = zod.object({
     title : zod.string().min(1),
     description : zod.string().min(1),
     completed : zod.boolean()
 })
 
-export default todoValidationSchema;
+ 
